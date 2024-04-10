420c5b6d1efe10271d5581c5043e4934743536f9
lalanikw <lalanikw@gmail.com>
2024-04-10 12:08:03 +0530

    adding a 404 page

--- app/not-found.js
+++ app/not-found.js
@@ -2,9 +2,9 @@ import Link from 'next/link';
 
 export default function NotFound() {
   return (
-    <div className=" h-screen">
-      <h1 className="p-20 text-4xl text-center text-black-700 tracking-widest font-semibold " >404 | Page Not Found</h1>
-      <p className='text-center p-10 text-2xl'> Please return <Link href="/" className='text-xl font-bold text-blue-700'> Home </Link></p>
+    <div className="p-60 h-screen">
+      <h1 className="p-18 text-4xl text-center text-black-700 tracking-widest font-semibold " >404 | Page Not Found</h1>
+      <p className='text-center p-10 text-2xl'> Please return <Link href="/" className='p-3 text-3xl font-bold transition-all ease-in-out cursor-pointer text-green-600 hover:text-2xl'> Home </Link></p>
     </div>
   );
 };
